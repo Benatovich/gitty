9d944148484186ff56f297e8eb3ae27d9fcf3aea
Benatovich <sambenatovich@gmail.com>
2022-04-01 15:02:33 -0400

    added secrets to github and updated .env and .yml files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,18 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/middleware/authenticate.js', () => {
+  return (req, res, next) => {
+    req.user = {
+      username: 'test_user',
+      photoUrl: 'http://image.com/image.png',
+    };
+
+    next();
+  };
+});
+
+
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,4 +23,14 @@ describe('gitty routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('redirects to the github oauth page upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+
+  
 });
