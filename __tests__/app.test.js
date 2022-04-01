5c8042ca8b614ba218f7e3c267a546fcfad952fc
Benatovich <sambenatovich@gmail.com>
2022-04-01 19:24:28 -0400

    callback test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
+const Post = require('../lib/models/Post');
 
 jest.mock('../lib/utils/github');
 const mockUser = {
@@ -39,19 +40,20 @@ describe('gitty routes', () => {
     );
   });
 
-  it('should login and test callback endpoint', async () => {
+  it.only('should login and test callback endpoint', async () => {
     const req = await request
       .agent(app)
       .get('/api/v1/github/login/callback?code=42')
       .redirects(1);
-
-    expect(req.body).toEqual({
-      username: '',
-      photoUrl: '',
-    });
+    console.log('|req.body', req.body);
+    expect(req.body).toEqual([{
+      id: '1',
+      text: 'test post', 
+      username: 'mockUser'
+    }]);
   });
 
-  it.only('logs a user out/deletes the session cookie', async () => {
+  it('logs a user out/deletes the session cookie', async () => {
     const agent = request.agent(app);
     const res = await agent.delete('/api/v1/github');
     const expected = {
@@ -62,4 +64,21 @@ describe('gitty routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+
+  it('returns a list of posts for all users', async () => {
+    // await Post.insert({ text: 'example' });
+    const agent = request.agent(app);
+    const res = await agent
+      .get('/api/v1/posts');
+
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      text: 'example',
+    }]);
+  });
+
+
+  // it('creates a post via POST', async () => {
+  //   await 
+  // })
 });
