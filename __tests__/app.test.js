84ca438f8781f63c4de92bc0fc1395c6ecdac486
Benatovich <sambenatovich@gmail.com>
2022-04-01 20:15:42 -0400

    cleaned up code, removed comments and console.logs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,16 +2,12 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
-const Post = require('../lib/models/Post');
-const GithubUser = require('../lib/models/GithubUser');
 
 jest.mock('../lib/utils/github');
 const mockUser = {
   username: 'mockUser',
   photoUrl: 'mockPhotoUrl'
 };
-
 
 describe('gitty routes', () => {
   beforeEach(() => {
@@ -36,7 +32,6 @@ describe('gitty routes', () => {
     const req = await request(app).get('/api/v1/github/login');
 
     expect(req.header.location).toMatch(
-      // /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
       'https://github.com/login/oauth/authorize?client_id=cacb474b27a5efc74450&scope=user&redirect_uri=http://localhost:7890/api/v1/auth/login/callback'
     );
   });
@@ -67,7 +62,6 @@ describe('gitty routes', () => {
 
 
   it('returns a list of posts for all users', async () => {
-    // await Post.insert({ text: 'example' });
     const agent = request.agent(app);
     await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
     
